11070b875ffd46776b9596157002176c52c09e25
Alessandro Carminati (Red Hat) <alessandro.carminati@gmail.com>
2023-08-29 07:30:37 +0000

    Modules: Introduce boot-time module signature flexibility
    This commit introduces a novel boot argument parameter that provides an
    advanced level of control over the verification of module signatures
    during the initial stages of booting. With this enhancement, we gain the
    capability to postpone the verification of module signatures to after
    intrd stage is finished.
    
    Given that bootloader-provided artifacts are commonly employed
    post-verification, we can effectively capitalize on this verification
    process and exempt the verification of modules within the initrd.
    This strategic adjustment reduces the time needed in this stage to load
    modules by temporarily enabling unsigned modules.
    Before terminating the stage modules signature check can be re-enabled
    keeping the system security level.
    
    To activate this functionality, a fresh boot argument parameter has
    been incorporated to disable verification during early boot.
    Furthermore, a supplementary method is necessary to reactivate
    verification subsequently.
    
    This is executed by leveraging a switch within a /proc file, inspired
    by the foundational principles of the Lockdown LSM levels. While this
    mechanism permits the activation of the functionaliity, and it ensures
    that the switch remains unalterable once engaged.
    
    example usage:
    ```
    $ qemu-system-aarch64 -M virt -cpu cortex-a53 -machine gic-version=3 \
                   -m 512 -nographic -smp 2 -kernel arch/arm64/boot/Image \
                   -initrd ../lka64/aarch64br.initrd \
                   -append "rootwait init=/sbin/init module_sig_check_wait=1"
    
    [    0.000000] Booting Linux on physical CPU 0x0000000000 [0x410fd034]
    [...]
    [    0.689441] Freeing unused kernel memory: 8576K
    [    0.690536] Run /init as init process
    Starting syslogd: OK
    Starting klogd: OK
    Running sysctl: OK
    Saving 256 bits of non-creditable seed for next boot
    Starting network: OK
    
    Welcome to Buildroot
    buildroot login: root
    ~ # cat /sys/module/module/parameters/sig_enforce
    Y
    ~ # cat /proc/module_sig_check_wait
    1
    ~ # insmod usbserial.ko
    ~ # lsmod
    Module                  Size  Used by    Not tainted
    usbserial              36864  0
    ~ # rmmod usbserial
    ~ # echo 0 >/proc/module_sig_check_wait
    [  226.114569] module_sig_check_wait disabled!
    ~ # insmod usbserial.ko
    [  230.646980] Loading of unsigned module is rejected
    [  230.648608] Loading of unsigned module is rejected
    insmod: can't insert 'usbserial.ko': Key was rejected by service
    ~ # echo 1 >/proc/module_sig_check_wait
    [  248.036518] module_sig_check_wait can be only disabled!
    ~ #
    ```
    
    Signed-off-by: Alessandro Carminati (Red Hat) <alessandro.carminati@gmail.com>

--- kernel/module/signing.c
+++ kernel/module/signing.c
@@ -12,12 +12,15 @@
 #include <linux/string.h>
 #include <linux/verification.h>
 #include <linux/security.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
 #include <crypto/public_key.h>
 #include <uapi/linux/module.h>
 #include "internal.h"
 
 #undef MODULE_PARAM_PREFIX
 #define MODULE_PARAM_PREFIX "module."
+int module_sig_check_wait;
 
 static bool sig_enforce = IS_ENABLED(CONFIG_MODULE_SIG_FORCE);
 module_param(sig_enforce, bool_enable_only, 0644);
@@ -36,6 +39,59 @@ void set_module_sig_enforced(void)
 {
 	sig_enforce = true;
 }
+
+/*
+ * test thing to enable sig enforcing later in boot sequence
+ */
+static int __init module_sig_check_wait_arg(char *str)
+{
+	return kstrtoint(str, 0, &module_sig_check_wait);
+}
+__setup("module_sig_check_wait=", module_sig_check_wait_arg);
+
+static int module_sig_check_wait_read(struct seq_file *m, void *v)
+{
+	seq_printf(m, "%d\n", module_sig_check_wait);
+	return 0;
+}
+
+static ssize_t module_sig_check_wait_write(struct file *file, const char __user *ubuf,
+					   size_t count, loff_t *ppos)
+{
+	int tmp;
+
+	if (kstrtoint_from_user(ubuf, count, 10, &tmp))
+		return -EINVAL;
+	if (tmp != 0) {
+		pr_info("module_sig_check_wait can be only disabled!\n");
+		return -EINVAL;
+		}
+	pr_info("module_sig_check_wait disabled!\n");
+	module_sig_check_wait = tmp;
+
+	return count;
+}
+
+static int module_sig_check_wait_proc_open(struct inode *inode, struct  file *file)
+{
+	return single_open(file, module_sig_check_wait_read, NULL);
+}
+
+static const struct proc_ops module_sig_check_wait_ops = {
+	.proc_open      = module_sig_check_wait_proc_open,
+	.proc_read      = seq_read,
+	.proc_lseek     = seq_lseek,
+	.proc_release   = single_release,
+	.proc_write     = module_sig_check_wait_write,
+};
+
+static int __init module_sig_check_wait_init(void)
+{
+	proc_create("module_sig_check_wait", 0, NULL,
+		    &module_sig_check_wait_ops);
+	return 0;
+}
+module_init(module_sig_check_wait_init);
 
 /*
  * Verify the signature on a module.
@@ -69,6 +125,8 @@ int mod_verify_sig(const void *mod, struct load_info *info)
 
 int module_sig_check(struct load_info *info, int flags)
 {
+	if (module_sig_check_wait)
+		return 0;
 	int err = -ENODATA;
 	const unsigned long markerlen = sizeof(MODULE_SIG_STRING) - 1;
 	const char *reason;
