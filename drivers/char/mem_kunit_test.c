a9ac44d89b68df14b54431ad55eb37fba3d7739e
Alessandro Carminati <acarmina@redhat.com>
2025-11-16 07:41:25 +0100

    more tests
    Signed-off-by: Alessandro Carminati <acarmina@redhat.com>

--- drivers/char/mem_kunit_test.c
+++ drivers/char/mem_kunit_test.c
@@ -22,8 +22,70 @@ static const char *mem_test_plan = "x86-strict";
 static const char *mem_test_plan = "x86-nonstrict";
 #elif defined(CONFIG_STRICT_DEVMEM)
 #else
-static const char *mem_test_plan = "generic-nonstrict"";
+static const char *mem_test_plan = "generic";
 #endif
+
+static void kunit_hexdump_page(struct kunit *test, const void *addr)
+{
+	const u8 *p = addr;
+	size_t offset = 0;
+
+	for (offset = 0; offset < PAGE_SIZE; offset += 16) {
+		char line[80];
+		int pos = 0;
+		int i;
+
+		pos += scnprintf(line + pos, sizeof(line) - pos,
+				 "%08zx: ", offset);
+
+		for (i = 0; i < 16; i++) {
+			pos += scnprintf(line + pos, sizeof(line) - pos,
+					 "%02x ", p[offset + i]);
+		}
+
+		pos += scnprintf(line + pos, sizeof(line) - pos, " |");
+
+		for (i = 0; i < 16; i++) {
+			u8 c = p[offset + i];
+			pos += scnprintf(line + pos, sizeof(line) - pos,
+					 "%c", ((c>31) && (c<127)) ? c : '.');
+		}
+
+		pos += scnprintf(line + pos, sizeof(line) - pos, "|");
+
+		kunit_info(test, "%s\n", line);
+	}
+}
+
+static void kunit_hexdump_compare_page(struct kunit *test,
+                                       const void *a,
+                                       const void *b)
+{
+	const u8 *pa = a;
+	const u8 *pb = b;
+	size_t offset;
+
+	for (offset = 0; offset < PAGE_SIZE; offset += 16) {
+		char line[160];
+		int pos = 0;
+		int i;
+
+		pos += scnprintf(line + pos, sizeof(line) - pos,
+				 "%08zx: ", offset);
+
+		for (i = 0; i < 16; i++)
+			pos += scnprintf(line + pos, sizeof(line) - pos,
+					 "%02x ", pa[offset + i]);
+
+		pos += scnprintf(line + pos, sizeof(line) - pos, " | ");
+
+		for (i = 0; i < 16; i++)
+			pos += scnprintf(line + pos, sizeof(line) - pos,
+					 "%02x ", pb[offset + i]);
+
+		kunit_info(test, "%s\n", line);
+	}
+}
 
 static int mem_test_init(struct kunit *test)
 {
@@ -68,7 +130,7 @@ static void read_mem_valid_basic_test(struct kunit *test)
 	KUNIT_EXPECT_GE(test, ret, 0);
 }
 
-static void read_mem_x86_read_1M_le(struct kunit *test)
+static void read_mem_read_1M_le_at_0(struct kunit *test)
 {
 	struct file fake_file = { };
 	struct mem_test_ctx *ctx = test->priv;
@@ -83,10 +145,39 @@ static void read_mem_x86_read_1M_le(struct kunit *test)
 
 	KUNIT_ASSERT_EQ(test, ret, PAGE_SIZE / 2);
 
+//	kunit_hexdump_page(test, ctx->umem);
+//	kunit_hexdump_compare_page(test, ctx->umem, direct);
 	KUNIT_EXPECT_MEMEQ(test, ctx->umem, direct, PAGE_SIZE / 2);
 }
 
-static void read_mem_strict_devmem(struct kunit *test)
+static void read_mem_read_1M_le_at_ram(struct kunit *test)
+{
+	struct file fake_file = { };
+	struct mem_test_ctx *ctx = test->priv;
+	phys_addr_t pa = 0x20000;
+	loff_t pos = pa;
+	void *direct;
+	int ret;
+
+	direct = phys_to_virt(pa);
+
+	memset(direct, 0xaa, PAGE_SIZE / 2);
+
+	ret = read_mem(&fake_file, ctx->umem, PAGE_SIZE / 2, &pos);
+
+//	kunit_hexdump_compare_page(test, ctx->umem, direct);
+	KUNIT_ASSERT_EQ(test, ret, PAGE_SIZE / 2);
+
+#if defined(CONFIG_X86) && defined(CONFIG_STRICT_DEVMEM)
+	kunit_info(test, "Expect different\n");
+	KUNIT_EXPECT_MEMNEQ(test, ctx->umem, direct, PAGE_SIZE / 2);
+#else
+	kunit_info(test, "Expect equal\n");
+	KUNIT_EXPECT_MEMEQ(test, ctx->umem, direct, PAGE_SIZE / 2);
+#endif
+}
+
+static void read_mem_less_page(struct kunit *test)
 {
 	struct file fake_file = { };
 	struct mem_test_ctx *ctx = test->priv;
@@ -97,30 +188,19 @@ static void read_mem_strict_devmem(struct kunit *test)
 
 	memset(buf, 0xaa, PAGE_SIZE / 2);
 
-	ret = read_mem(&fake_file, ctx->umem, PAGE_SIZE /2, &pos);
+	ret = read_mem(&fake_file, ctx->umem, PAGE_SIZE / 2, &pos);
 
+#if defined(CONFIG_STRICT_DEVMEM)
+	kunit_info(test, "expected error\n");
 	KUNIT_ASSERT_EQ(test, ret, -EPERM);
+#else
+	kunit_info(test, "expected read\n");
+	KUNIT_ASSERT_EQ(test, ret, PAGE_SIZE / 2);
+	KUNIT_EXPECT_MEMEQ(test, ctx->umem, buf, PAGE_SIZE / 2);
+#endif
 }
 
-static void read_mem_nostrict_devmem(struct kunit *test)
-{
-	struct file fake_file = { };
-	struct mem_test_ctx *ctx = test->priv;
-	char *buf = kunit_kmalloc(test, PAGE_SIZE, GFP_KERNEL);
-	phys_addr_t pa = virt_to_phys(buf);
-	loff_t pos = pa;
-	int ret;
-
-	memset(buf, 0xaa, PAGE_SIZE);
-
-	ret = read_mem(&fake_file, ctx->umem, PAGE_SIZE, &pos);
-
-	KUNIT_ASSERT_EQ(test, ret, PAGE_SIZE);
-
-	KUNIT_EXPECT_MEMEQ(test, ctx->umem, buf, PAGE_SIZE);
-}
-
-static void read_mem_nostrict_devmem_2p(struct kunit *test)
+static void read_mem_2_pages(struct kunit *test)
 {
 	struct file fake_file = { };
 	struct mem_test_ctx *ctx = test->priv;
@@ -135,17 +215,24 @@ static void read_mem_nostrict_devmem_2p(struct kunit *test)
 	ret = read_mem(&fake_file, ctx->umem, 2 * PAGE_SIZE, &pos);
 	kunit_info(test, "read_mem read %d\n", ret);
 
+#if defined(CONFIG_STRICT_DEVMEM)
+	kunit_info(test, "expected error\n");
+	KUNIT_ASSERT_EQ(test, ret, -EPERM);
+#else
+	kunit_info(test, "expected read\n");
 	KUNIT_ASSERT_EQ(test, ret, 2 * PAGE_SIZE);
 
 	KUNIT_EXPECT_MEMEQ(test, ctx->umem, buf, 2 * PAGE_SIZE);
+#endif
 }
 
-#if defined(CONFIG_X86) && defined(CONFIG_STRICT_DEVMEM)
 static struct kunit_case mem_cases[] = {
 	KUNIT_CASE_PARAM(read_mem_invalid_offset_test, NULL),
 	KUNIT_CASE_PARAM(read_mem_valid_basic_test, NULL),
-	KUNIT_CASE_PARAM(read_mem_x86_read_1M_le, NULL),
-	KUNIT_CASE_PARAM(read_mem_strict_devmem, NULL),
+	KUNIT_CASE_PARAM(read_mem_read_1M_le_at_0, NULL),
+	KUNIT_CASE_PARAM(read_mem_read_1M_le_at_ram, NULL),
+	KUNIT_CASE_PARAM(read_mem_less_page, NULL),
+	KUNIT_CASE_PARAM(read_mem_2_pages, NULL),
 	{}
 };
 
@@ -156,39 +243,3 @@ static struct kunit_suite mem_suite = {
 };
 
 kunit_test_suite(mem_suite);
-
-#elif defined(CONFIG_X86)
-static struct kunit_case mem_cases[] = {
-	KUNIT_CASE_PARAM(read_mem_invalid_offset_test, NULL),
-	KUNIT_CASE_PARAM(read_mem_valid_basic_test, NULL),
-	KUNIT_CASE_PARAM(read_mem_x86_read_1M_le, NULL),
-	KUNIT_CASE_PARAM(read_mem_nostrict_devmem, NULL),
-	KUNIT_CASE_PARAM(read_mem_nostrict_devmem_2p, NULL),
-	{}
-};
-
-static struct kunit_suite mem_suite = {
-	.name = "devmem-read_mem",
-	.init = mem_test_init,
-	.test_cases = mem_cases,
-};
-
-kunit_test_suite(mem_suite);
-
-#else
-static struct kunit_case mem_cases[] = {
-	KUNIT_CASE_PARAM(read_mem_invalid_offset_test, NULL),
-	KUNIT_CASE_PARAM(read_mem_valid_basic_test, NULL),
-	KUNIT_CASE_PARAM(read_mem_nostrict_devmem, NULL),
-	{}
-};
-
-static struct kunit_suite mem_suite = {
-	.name = "devmem-read_mem",
-	.init = mem_test_init,
-	.test_cases = mem_cases,
-};
-
-kunit_test_suite(mem_suite);
-
-#endif
