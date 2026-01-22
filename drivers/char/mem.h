7314a91fae7b8ab582d30bf5a315f0152b87dd16
Alessandro Carminati <acarmina@redhat.com>
2026-01-22 22:55:26 +0100

    DEVMEM_KUNIT_TEST_IO
    Signed-off-by: Alessandro Carminati <acarmina@redhat.com>

--- drivers/char/mem.h
+++ drivers/char/mem.h
@@ -11,6 +11,7 @@ ssize_t read_mem(struct file *file, char __user *buf,
 			size_t count, loff_t *ppos);
 ssize_t write_mem(struct file *file, const char __user *buf,
 			 size_t count, loff_t *ppos);
+int page_is_allowed(unsigned long pfn);
 #endif
 
 #endif /* _LINUX_CHAR_MEM_H */
